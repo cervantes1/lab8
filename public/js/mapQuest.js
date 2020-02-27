eb9a0d15c9b56b17400b0dfc976cfcc2880ee347
cervantes1 <rgcervan@ucsd.edu>
2020-02-26 18:41:00 -0800

    map quest api

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,10 @@
 function initMap() {
 	// add your code here
-}
+	L.mapquest.key = 'WROMII0dHk5NQgMKVdOMi6zGJObd59VV';
+
+	// 'map' refers to a <div> element with the ID map
+	L.mapquest.map('map', {
+	  center: [32.8801, -117.2340],
+	  layers: L.mapquest.tileLayer('map'),
+	  zoom: 12
+	});}
